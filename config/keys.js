14967dfe31ea59e18cebd6a5bb0285d8d85f8ced
Kyle Desktop <kylespresser@gmail.com>
2018-05-29 11:01:36 -0400

    lecture 12 add register user route and jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://key_user:key123@ds235860.mlab.com:35860/keyspress_devconnector"
+    'mongodb://key_user:key123@ds235860.mlab.com:35860/keyspress_devconnector',
+  secretOrKey: 'secret'
 };
